0f58b8eaaa6ac3f86aa7d450008c36f0bd5d3c85
Temitope Dickson Olajide <77141335+Otopxy@users.noreply.github.com>
2025-01-12 19:29:00 -0600

    Create Projects.js

---
+++ src/components/Projects.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Projects() {
+  return (
+    <section className="projects">
+      <h2>Projects</h2>
+      <div>
+        <i>
+        <p>Blockchain Based Ticketing System.</p>
+        <p>Blockchain Wallet Tracking System.</p>
+        <p>Sport Item MarketPlace.</p>
+        <p>Agriculture MarketPlace.</p>
+      </i>
+      </div>
+    </section>
+  );
+}
+
+export default Projects;
