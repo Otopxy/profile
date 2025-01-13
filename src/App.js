3d3fa0bfab702783b44147865f5e6ed0c6f20ea8
Temitope Dickson Olajide <77141335+Otopxy@users.noreply.github.com>
2025-01-12 19:26:09 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import WorkExperience from "./components/WorkExperience";
 import Education from "./components/Education";
 import Certifications from "./components/Certifications";
 import Contact from "./components/Contact";
-import ImageUpload from "./components/ImageUpload";
+//import ImageUpload from "./components/ImageUpload";
 import './App.css';
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Skills />
-      <WorkExperience />
       <Education />
+      <WorkExperience />
       <Certifications />
+      <Projects />
       <Contact />
     </div>
   );
