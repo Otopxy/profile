1d82f6e203f1a0eca297f5ba65cfbeb2e4c24b0f
Temitope Dickson Olajide <77141335+Otopxy@users.noreply.github.com>
2025-01-12 19:29:31 -0600

    Update Certifications.js

--- src/components/Certifications.js
+++ src/components/Certifications.js
@@ -5,13 +5,22 @@ function Certifications() {
     <section className="certifications">
       <h2>Training & Certifications</h2>
       <ul>
-        <li>GDPR Data Protection Officer Skills - University of Derby Online Learning</li>
-        <li>IBM Blockchain Foundation Developer - IBM</li>
+        <li>Operating Systems.</li>
+        <li>Computer Networking.</li>
+        <li>Software Engineering.</li>
+        <li>Computer Architecture.</li>
+        <li>Circuit Theory and Electronics.</li>
+        <li>Computer and Information Security.</li>
+        <li>AWS Cloud Quest: Cloud Practitioner.</li>
+        <li>Digital Skills : Social Media – Accenture.</li>
+        <li>Artificial Intelligence and Expert systems.</li>
+        <li>IBM Blockchain Foundation Developer - IBM.</li>
+        <li>NanjingUniversity, China - Translation in Practice</li>
         <li>Software Development Career Progression Program - elev8 | Microsoft</li>
         <li>Data Structures & Algorithms, AI, Object-Oriented Programming (Java, C++, C#)</li>
+        <li>GDPR Data Protection Officer Skills - University of Derby Online Learning</li>
         <li>National Research Tomsk State University and Palex, Russia - Translation quality management</li>      
-        <li>NanjingUniversity, China - Translation in Practice</li>
-      </ul>
+        </ul>
     </section>
   );
 }
