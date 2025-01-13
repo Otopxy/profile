eb396920b2b4a1f65d2ed6959f06218ab3768722
Temitope Dickson Olajide <77141335+Otopxy@users.noreply.github.com>
2025-01-12 19:26:51 -0600

    Update WorkExperience.js

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -4,67 +4,78 @@ function WorkExperience() {
   return (
     <section className="work-experience">
       <h2>Work Experience</h2>
-      
-        <h3>Library Computer Services (WIU) - Student Assistant</h3>
-        <p><strong>Macomb, IL (2024 - Present)</strong></p>
-         <ul>
-          <li>Diagnosed and resolved software and hardware issues for library computers.</li>
+
+        <h3>College of Business and Technology Dean’s Office (WIU) - Social Media/Web Developer - Graduate Assistant</h3>
+        <p><strong>Macomb, IL (2025 - Present)</strong></p>
+         <ul><i>
+          <li>Ensured website accessibility and compliance with web standards.</li>
+          <li>Created and updated website content, including text, images, and multimedia.</li>
+          <li>Created high-quality, visually appealing, and engaging social media content, including text, images, and videos.</li>
+          <li>Tracked and analyzed social media metrics to measure campaign effectiveness and identify areas for improvement.</li>
+          <li>Assist in the development and maintenance of college websites and other digital platforms using HTML, CSS, and Javascript.</li>
+          <li>Developed and executed social media strategies for various platforms to increase brand awareness, engagement, and student recruitment.</li>
+          </i></ul>
+        <br></br>
+        <h3>Library Computer Services (WIU) - Computer Technician - Student Assistant</h3>
+        <p><strong>Macomb, IL (2024)</strong></p>
+         <ul><i>
           <li>Performed routine software updates to maintain system security.</li>
           <li>Provided technical support and training to library staff and students.</li>
+          <li>Diagnosed and resolved software and hardware issues for library computers.</li>
           <li>Documented processes and system configurations to ensure clear communication and
           knowledge transfer.</li>
-         </ul>
+         </i></ul>
         <br></br>
-        <h3>EntityBlock, Inc - Blockchain Developer</h3>
+        <h3>EntityBlock, Inc - Blockchain Developer/Chief Technology Officer</h3>
         <p><strong>Miami, FL (2022 - 2023)</strong></p>
-        <ul>
-          <li>Worked on blockchain technology projects and maintained front-end/back-end apps.</li>
-          <li>Upgraded NFTs from ERC-721 to ERC-1155 standards.</li>
-          <li>Participated in technical specification design, technical roadmap, and implementation of
-          product.</li>
-          <li>Performed and assisted team members through code reviews, acceptance testing, bug
-          fixing, and general support.</li>
+        <ul><i>
+          <li>Maintained back-end and front-end application.</li>
+          <li>Upgraded NFTs project from ERC-721 to ERC-1155 standards.</li>
+          <li>Mentored others on in-depth Blockchain/EVM topics and best practices.</li>
           <li>Collaborated with managers to determine blockchain technology needs and envisaged
           functionalities.</li>
-        </ul>
+          <li>Participated in technical specification design, technical roadmap, and implementation of product.</li>
+          <li>Performed and assisted team members through code reviews, acceptance testing, bug
+          fixing, and general support.</li>
+          </i></ul>
         <br></br>
         <h3>Mozilla Firefox - Yoruba Translator and Contributor</h3>
         <p><strong>Remote Volunteer, NG(2020 - 2023)</strong></p>
-        <ul>
-          <li>Review Yoruba language field suggestion from contributors and provide timely feedback that
-          focuses on developing skills.</li>
-          <li>Work with managersto create and maintain language resources for the community.</li>
+        <ul><i>
           <li>Report issues concerning source language content.</li>
           <li>Assist community locale manager and help resolve enquiries.</li>
-        </ul>
+          <li>Work with managersto create and maintain language resources for the community.</li>
+          <li>Review Yoruba language field suggestion from contributors and provide timely feedback that
+          focuses on developing skills.</li>
+          </i></ul>
         <br></br>
         <h3>MICROSOFT ATO/ELEV8 - Developer Trainee</h3>
         <p><strong>Lagos, NG(2022)</strong></p>
-        <ul>
-          <li>Designed and implemented a web application using .NET, enhancing connectivity between
-          farmers and consumers.</li>
+        <ul><i>
+          <li>Designed and implemented a web application using C#.NET.</li>
           <li>Wrote and maintained clean, efficient code across various projects, ensuring high code
           quality and maintainability.</li>
-          <li>Defined, manipulated, and managed databases using MSSQL and NoSQL (MongoDB),
-          optimizing data storage and retrieval.</li>
-          <li>Developed applications using Node.js and Python, expanding technical expertise in
-          multiple programming environments.</li>
           <li>Worked on Azure-based AI and machine learning projects, applying advanced technologies
           to solve complex problems.</li>
-        </ul>
+          <li>Developed applications using Node.js and Python, expanding technical expertise in
+          multiple programming environments.</li>
+          <li>Defined, manipulated, and managed databases using MSSQL and NoSQL (MongoDB),
+          optimizing data storage and retrieval.</li>
+          <li>Worked on designing, developing, and integrating RESTful APIs to facilitate seamless communication between software components and third-party services.</li>
+          </i></ul>
         <br></br>
         <h3>UPTITEK Touch of Simplicity - IT Officer</h3>
         <p><strong>Owerri, NG (2021)</strong></p>
-        <ul>
-          <li>Provided technical support by diagnosing and resolving software issues, collaborating
-          closely with developers to troubleshoot and fix bugs in applications.</li>
+        <ul><i>
+          <li>Participated in technical specification design, technical roadmap, and implementation of
+          product.</li>
           <li>Executed scripts for debugging and automating tasks within EMB ERP systems, enhancing
           system efficiency and reliability.</li>
           <li>Worked on optimizing and maintaining EMB ERP systems, contributing to the
           improvement of system performance and user experience.</li>
-          <li>Participated in technical specification design, technical roadmap, and implementation of
-          product.</li>
-        
+          <li>Provided technical support by diagnosing and resolving software issues, collaborating
+          closely with developers to troubleshoot and fix bugs in applications.</li>
+          </i>
       </ul>
     </section>
   );
